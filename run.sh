9ec454d3428cff7504f638340bdba887be5757ee
mperry-dev <mperry_48@outlook.com>
2023-07-19 16:07:41 +1000

    remove redundant depency

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 # Generate application JAR file.
 mvn clean package
 
-# Build the Docker image:
+# Build the Docker image. Need to rerun when rebuild java package since it grabs JAR.
 docker build -t tournamaths .
 
 docker-compose up
