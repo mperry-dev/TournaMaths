255b61565d0fac7613af77a924c5b165e67a4982
mperry-dev <mperry_48@outlook.com>
2023-07-20 13:47:08 +1000

    more tweaks

--- src/main/java/com/tournamaths/controller/MathQuestionController.java
+++ src/main/java/com/tournamaths/controller/MathQuestionController.java
@@ -2,6 +2,8 @@ package com.tournamaths.controller;
 
 import com.tournamaths.entity.MathQuestion;
 import com.tournamaths.repository.MathQuestionRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MathQuestionController {
 
+    @Autowired
     private final MathQuestionRepository repository;
 
     public MathQuestionController(MathQuestionRepository repository) {
--- src/main/java/com/tournamaths/repository/MathQuestionRepository.java
+++ src/main/java/com/tournamaths/repository/MathQuestionRepository.java
@@ -4,6 +4,8 @@ import com.tournamaths.entity.MathQuestion;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 //import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MathQuestionRepository extends JpaRepository<MathQuestion, Long> {
 }
