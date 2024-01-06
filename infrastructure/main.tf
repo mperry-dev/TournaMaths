a7c279b613db8c1b123ec6319edf47292469cdc7
mperry-dev <mperry_48@outlook.com>
2024-01-06 11:20:05 +1000

    tweaks

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -18,8 +18,8 @@ resource "aws_vpc" "tourna_math_vpc" {
 
 ################ Public subnets for the application's EC2 instances.
 resource "aws_subnet" "tourna_math_subnet_1a" {
-  vpc_id     = aws_vpc.tourna_math_vpc.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id            = aws_vpc.tourna_math_vpc.id
+  cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
 
   tags = {
@@ -28,8 +28,8 @@ resource "aws_subnet" "tourna_math_subnet_1a" {
 }
 
 resource "aws_subnet" "tourna_math_subnet_1b" {
-  vpc_id     = aws_vpc.tourna_math_vpc.id
-  cidr_block = "10.0.2.0/24"
+  vpc_id            = aws_vpc.tourna_math_vpc.id
+  cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-1b"
 
   tags = {
@@ -39,8 +39,8 @@ resource "aws_subnet" "tourna_math_subnet_1b" {
 
 ################ Private subnets for database, with a subnet group that the database will be restricted to.
 resource "aws_subnet" "tourna_math_private_subnet_1a" {
-  vpc_id     = aws_vpc.tourna_math_vpc.id
-  cidr_block = "10.0.3.0/24"
+  vpc_id            = aws_vpc.tourna_math_vpc.id
+  cidr_block        = "10.0.3.0/24"
   availability_zone = "us-east-1a"
 
   tags = {
@@ -49,8 +49,8 @@ resource "aws_subnet" "tourna_math_private_subnet_1a" {
 }
 
 resource "aws_subnet" "tourna_math_private_subnet_1b" {
-  vpc_id     = aws_vpc.tourna_math_vpc.id
-  cidr_block = "10.0.4.0/24"
+  vpc_id            = aws_vpc.tourna_math_vpc.id
+  cidr_block        = "10.0.4.0/24"
   availability_zone = "us-east-1b"
 
   tags = {
@@ -146,7 +146,7 @@ resource "aws_lb_target_group" "tourna_math_tg" {
   vpc_id   = aws_vpc.tourna_math_vpc.id
 
   health_check {
-    enabled = true
+    enabled  = true
     interval = 30
     path     = "/"
     timeout  = 3
@@ -170,9 +170,9 @@ resource "aws_lb_listener" "front_end" {
 
 ################ Launch configuration.
 resource "aws_launch_configuration" "tourna_math_lc" {
-  name          = "TournaMaths-LC"
-  image_id      = "ami-053b0d53c279acc90"  # Ubuntu Server 22.04 LTS (HVM), SSD Volume Type (provided by Ubuntu)
-  instance_type = "t2.micro"  # A cheap instance which unlike t3.micro, doesn't have unlimited bursting, so is safer cost-wise.
+  name            = "TournaMaths-LC"
+  image_id        = "ami-053b0d53c279acc90" # Ubuntu Server 22.04 LTS (HVM), SSD Volume Type (provided by Ubuntu)
+  instance_type   = "t2.micro"              # A cheap instance which unlike t3.micro, doesn't have unlimited bursting, so is safer cost-wise.
   security_groups = [aws_security_group.tourna_math_sg.id]
 
   lifecycle {
@@ -188,18 +188,18 @@ resource "aws_launch_configuration" "tourna_math_lc" {
 
 ################ Autoscaling group for application.
 resource "aws_autoscaling_group" "tourna_math_asg" {
-  desired_capacity     = 2
-  max_size             = 5
-  min_size             = 1
+  desired_capacity          = 2
+  max_size                  = 5
+  min_size                  = 1
   health_check_grace_period = 300
-  health_check_type        = "EC2"
-  force_delete             = true
-  launch_configuration     = aws_launch_configuration.tourna_math_lc.name
-  vpc_zone_identifier      = [aws_subnet.tourna_math_subnet_1a.id, aws_subnet.tourna_math_subnet_1b.id]
+  health_check_type         = "EC2"
+  force_delete              = true
+  launch_configuration      = aws_launch_configuration.tourna_math_lc.name
+  vpc_zone_identifier       = [aws_subnet.tourna_math_subnet_1a.id, aws_subnet.tourna_math_subnet_1b.id]
 
   tag {
-    key = "Name"
-    value = "TournaMaths-ASG"
+    key                 = "Name"
+    value               = "TournaMaths-ASG"
     propagate_at_launch = true
   }
 }
@@ -207,7 +207,8 @@ resource "aws_autoscaling_group" "tourna_math_asg" {
 ################ Route53 configuration - records, and SSL certificate.
 
 resource "aws_route53_zone" "tournamaths_zone" {
-  name = "tournamaths.com"
+  name          = "tournamaths.com"
+  force_destroy = true
 }
 
 resource "aws_route53_record" "tournamaths_a_record" {
@@ -220,14 +221,6 @@ resource "aws_route53_record" "tournamaths_a_record" {
     zone_id                = aws_lb.tourna_math_alb.zone_id
     evaluate_target_health = true
   }
-}
-
-resource "aws_route53_record" "tournamaths_cname_record" {
-  zone_id = aws_route53_zone.tournamaths_zone.zone_id
-  name    = "subdomain.tournamaths.com"
-  type    = "CNAME"
-  ttl     = "300"
-  records = [aws_lb.tourna_math_alb.dns_name]
 }
 
 # Because certificates have to be validated, might need to run Terraform multiple times if replace certificate
@@ -260,11 +253,12 @@ resource "aws_route53_record" "tournamaths_cert_validation_record" {
     }
   }
 
-  zone_id = aws_route53_zone.tournamaths_zone.zone_id
-  name    = each.value.name
-  type    = each.value.type
-  records = [each.value.record]
-  ttl     = 60
+  allow_overwrite = true
+  zone_id         = aws_route53_zone.tournamaths_zone.zone_id
+  name            = each.value.name
+  type            = each.value.type
+  records         = [each.value.record]
+  ttl             = 600
 }
 
 resource "aws_acm_certificate_validation" "tournamaths_cert_validation" {
@@ -274,16 +268,16 @@ resource "aws_acm_certificate_validation" "tournamaths_cert_validation" {
 
 ################ Database.
 resource "aws_db_instance" "tourna_math_db" {
-  allocated_storage    = 20
-  storage_type         = "gp2"
-  engine               = "postgres"
-  engine_version       = "15.3"
-  instance_class       = "db.t4g.micro"
-  db_name              = "tourna_math_db"
-  username             = "admin_user"  # Can't say "admin" here as that's reserved in Postgres.
-  password             = jsondecode(data.aws_secretsmanager_secret_version.db_creds.secret_string)["db_admin_user_password"]
-  parameter_group_name = "default.postgres15"
-  skip_final_snapshot  = true
+  allocated_storage      = 20
+  storage_type           = "gp2"
+  engine                 = "postgres"
+  engine_version         = "15.3"
+  instance_class         = "db.t4g.micro"
+  db_name                = "tourna_math_db"
+  username               = "admin_user" # Can't say "admin" here as that's reserved in Postgres.
+  password               = jsondecode(data.aws_secretsmanager_secret_version.db_creds.secret_string)["db_admin_user_password"]
+  parameter_group_name   = "default.postgres15"
+  skip_final_snapshot    = true
   vpc_security_group_ids = [aws_security_group.tourna_math_sg.id]
   db_subnet_group_name   = aws_db_subnet_group.tourna_math_private_db_subnet_group.name
 
