9d790b2a1d40de4815f812c6ed620ddd53cc263a
mperry-dev <mperry_48@outlook.com>
2024-01-07 15:17:54 +1000

    open security group up for debugging

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -106,9 +106,9 @@ resource "aws_security_group" "tourna_math_sg" {
   vpc_id = aws_vpc.tourna_math_vpc.id
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
