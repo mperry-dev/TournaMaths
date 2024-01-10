22213cbd8bde697f87ccb40e4867c48e38fc9f96
mperry-dev <mperry_48@outlook.com>
2024-01-10 16:17:30 +1000

    break cycle

--- infrastructure/networking.tf
+++ infrastructure/networking.tf
@@ -114,10 +114,10 @@ resource "aws_security_group" "tournamaths_alb_sg" {
   }
 
   egress {
-    from_port       = 8080
-    to_port         = 8080
-    protocol        = "tcp"
-    security_groups = [aws_security_group.tournamaths_ec2_sg.id]
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Can't specify tournamaths_ec2_sg here as would have a cycle
   }
 
   tags = {
