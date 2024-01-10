a64dcebcba787abe7efd79a7f1858da2144a9d0a
mperry-dev <mperry_48@outlook.com>
2024-01-10 14:42:53 +1000

    tourna_math_ -> tournamaths_

--- infrastructure/codedeploy.tf
+++ infrastructure/codedeploy.tf
@@ -9,12 +9,12 @@ resource "aws_codedeploy_deployment_group" "tournamaths_deployment_group" {
   deployment_group_name = "tournamaths-deployment-group"
   service_role_arn      = aws_iam_role.codedeploy_role.arn
 
-  autoscaling_groups = [aws_autoscaling_group.tourna_math_asg.name]
+  autoscaling_groups = [aws_autoscaling_group.tournamaths_asg.name]
 
   # Load Balancer Integration
   load_balancer_info {
     target_group_info {
-      name = aws_lb_target_group.tourna_math_tg.name
+      name = aws_lb_target_group.tournamaths_tg.name
     }
   }
 
--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,5 +1,5 @@
 ################ VPC.
-resource "aws_vpc" "tourna_math_vpc" {
+resource "aws_vpc" "tournamaths_vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
@@ -8,8 +8,8 @@ resource "aws_vpc" "tourna_math_vpc" {
 }
 
 ################ Public subnets for the application's EC2 instances.
-resource "aws_subnet" "tourna_math_subnet_1a" {
-  vpc_id            = aws_vpc.tourna_math_vpc.id
+resource "aws_subnet" "tournamaths_subnet_1a" {
+  vpc_id            = aws_vpc.tournamaths_vpc.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "${var.aws_region}a"
 
@@ -21,8 +21,8 @@ resource "aws_subnet" "tourna_math_subnet_1a" {
   }
 }
 
-resource "aws_subnet" "tourna_math_subnet_1b" {
-  vpc_id            = aws_vpc.tourna_math_vpc.id
+resource "aws_subnet" "tournamaths_subnet_1b" {
+  vpc_id            = aws_vpc.tournamaths_vpc.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "${var.aws_region}b"
 
@@ -35,8 +35,8 @@ resource "aws_subnet" "tourna_math_subnet_1b" {
 }
 
 ################ Private subnets for database, with a subnet group that the database will be restricted to.
-resource "aws_subnet" "tourna_math_private_subnet_1a" {
-  vpc_id            = aws_vpc.tourna_math_vpc.id
+resource "aws_subnet" "tournamaths_private_subnet_1a" {
+  vpc_id            = aws_vpc.tournamaths_vpc.id
   cidr_block        = "10.0.3.0/24"
   availability_zone = "${var.aws_region}a"
 
@@ -45,8 +45,8 @@ resource "aws_subnet" "tourna_math_private_subnet_1a" {
   }
 }
 
-resource "aws_subnet" "tourna_math_private_subnet_1b" {
-  vpc_id            = aws_vpc.tourna_math_vpc.id
+resource "aws_subnet" "tournamaths_private_subnet_1b" {
+  vpc_id            = aws_vpc.tournamaths_vpc.id
   cidr_block        = "10.0.4.0/24"
   availability_zone = "${var.aws_region}b"
 
@@ -55,14 +55,14 @@ resource "aws_subnet" "tourna_math_private_subnet_1b" {
   }
 }
 
-resource "aws_db_subnet_group" "tourna_math_private_db_subnet_group" {
+resource "aws_db_subnet_group" "tournamaths_private_db_subnet_group" {
   name       = "tournamaths-private-db-subnet-group"
-  subnet_ids = [aws_subnet.tourna_math_private_subnet_1a.id, aws_subnet.tourna_math_private_subnet_1b.id]
+  subnet_ids = [aws_subnet.tournamaths_private_subnet_1a.id, aws_subnet.tournamaths_private_subnet_1b.id]
 }
 
 ################ Internet gateway.
-resource "aws_internet_gateway" "tourna_math_igw" {
-  vpc_id = aws_vpc.tourna_math_vpc.id
+resource "aws_internet_gateway" "tournamaths_igw" {
+  vpc_id = aws_vpc.tournamaths_vpc.id
 
   tags = {
     Name = "TournaMaths-IGW"
@@ -70,12 +70,12 @@ resource "aws_internet_gateway" "tourna_math_igw" {
 }
 
 ################ Route table with associations to subnets.
-resource "aws_route_table" "tourna_math_route_table" {
-  vpc_id = aws_vpc.tourna_math_vpc.id
+resource "aws_route_table" "tournamaths_route_table" {
+  vpc_id = aws_vpc.tournamaths_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.tourna_math_igw.id
+    gateway_id = aws_internet_gateway.tournamaths_igw.id
   }
 
   tags = {
@@ -84,20 +84,20 @@ resource "aws_route_table" "tourna_math_route_table" {
 }
 
 # route_table_association to public route table means the linked subnets are public.
-resource "aws_route_table_association" "tourna_math_route_table_association_1a" {
-  subnet_id      = aws_subnet.tourna_math_subnet_1a.id
-  route_table_id = aws_route_table.tourna_math_route_table.id
+resource "aws_route_table_association" "tournamaths_route_table_association_1a" {
+  subnet_id      = aws_subnet.tournamaths_subnet_1a.id
+  route_table_id = aws_route_table.tournamaths_route_table.id
 }
 
-resource "aws_route_table_association" "tourna_math_route_table_association_1b" {
-  subnet_id      = aws_subnet.tourna_math_subnet_1b.id
-  route_table_id = aws_route_table.tourna_math_route_table.id
+resource "aws_route_table_association" "tournamaths_route_table_association_1b" {
+  subnet_id      = aws_subnet.tournamaths_subnet_1b.id
+  route_table_id = aws_route_table.tournamaths_route_table.id
 }
 
 ################ Security group.
-resource "aws_security_group" "tourna_math_ec2_sg" {
+resource "aws_security_group" "tournamaths_ec2_sg" {
   name   = "tournamath-ec2-sg"
-  vpc_id = aws_vpc.tourna_math_vpc.id
+  vpc_id = aws_vpc.tournamaths_vpc.id
 
   ingress {
     from_port   = 8080 # Port 8080 since Tomcat listening on port 8080
@@ -118,15 +118,15 @@ resource "aws_security_group" "tourna_math_ec2_sg" {
   }
 }
 
-resource "aws_security_group" "tourna_math_rds_sg" {
+resource "aws_security_group" "tournamaths_rds_sg" {
   name   = "tournamath-rds-sg"
-  vpc_id = aws_vpc.tourna_math_vpc.id
+  vpc_id = aws_vpc.tournamaths_vpc.id
 
   ingress {
     from_port       = 5432
     to_port         = 5432
     protocol        = "tcp"
-    security_groups = [aws_security_group.tourna_math_ec2_sg.id]
+    security_groups = [aws_security_group.tournamaths_ec2_sg.id]
   }
 
   tags = {
@@ -135,21 +135,21 @@ resource "aws_security_group" "tourna_math_rds_sg" {
 }
 
 ################ ALB, Target-Group, Listener.
-resource "aws_lb" "tourna_math_alb" {
+resource "aws_lb" "tournamaths_alb" {
   name               = "TournaMaths-ALB"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.tourna_math_ec2_sg.id]
-  subnets            = [aws_subnet.tourna_math_subnet_1a.id, aws_subnet.tourna_math_subnet_1b.id]
+  security_groups    = [aws_security_group.tournamaths_ec2_sg.id]
+  subnets            = [aws_subnet.tournamaths_subnet_1a.id, aws_subnet.tournamaths_subnet_1b.id]
 
   enable_deletion_protection = false
 }
 
-resource "aws_lb_target_group" "tourna_math_tg" {
+resource "aws_lb_target_group" "tournamaths_tg" {
   name     = "TournaMaths-TG"
   port     = 8080 # Tomcat (with SpringBoot) listens on port 8080 by default
   protocol = "HTTP"
-  vpc_id   = aws_vpc.tourna_math_vpc.id
+  vpc_id   = aws_vpc.tournamaths_vpc.id
 
   health_check {
     enabled  = true
@@ -160,7 +160,7 @@ resource "aws_lb_target_group" "tourna_math_tg" {
 }
 
 resource "aws_lb_listener" "http_listener" {
-  load_balancer_arn = aws_lb.tourna_math_alb.arn
+  load_balancer_arn = aws_lb.tournamaths_alb.arn
   port              = 80
   protocol          = "HTTP"
 
@@ -177,12 +177,12 @@ resource "aws_lb_listener" "http_listener" {
 }
 
 ################ Launch configuration.
-resource "aws_launch_template" "tourna_math_lt" {
+resource "aws_launch_template" "tournamaths_lt" {
   name_prefix   = "TournaMaths-LT-"
   image_id      = "ami-079db87dc4c10ac91" # Amazon Linux 2023 AMI (chose because optimized for AWS and comes with extra apps, also better documented)
   instance_type = "t3.micro"              # A cheap instance which is built on Nitro System, so can connect via EC2 Serial Console.
 
-  vpc_security_group_ids = [aws_security_group.tourna_math_ec2_sg.id]
+  vpc_security_group_ids = [aws_security_group.tournamaths_ec2_sg.id]
 
   # Disables T3 Unlimited feature so costs don't go up (not too expensive though https://aws.amazon.com/ec2/instance-types/t3/)
   credit_specification {
@@ -251,7 +251,7 @@ resource "aws_launch_template" "tourna_math_lt" {
 }
 
 ################ Autoscaling group for application.
-resource "aws_autoscaling_group" "tourna_math_asg" {
+resource "aws_autoscaling_group" "tournamaths_asg" {
   name                      = "TournaMaths-ASG"
   desired_capacity          = 2
   max_size                  = 5
@@ -259,13 +259,13 @@ resource "aws_autoscaling_group" "tourna_math_asg" {
   health_check_grace_period = 300
   health_check_type         = "EC2"
   force_delete              = true
-  vpc_zone_identifier       = [aws_subnet.tourna_math_subnet_1a.id, aws_subnet.tourna_math_subnet_1b.id]
+  vpc_zone_identifier       = [aws_subnet.tournamaths_subnet_1a.id, aws_subnet.tournamaths_subnet_1b.id]
 
-  target_group_arns = [aws_lb_target_group.tourna_math_tg.arn]
+  target_group_arns = [aws_lb_target_group.tournamaths_tg.arn]
 
   launch_template {
-    id      = aws_launch_template.tourna_math_lt.id
-    version = aws_launch_template.tourna_math_lt.latest_version # Specify this instead of "$Latest" so instance refresh triggered when launch template changes
+    id      = aws_launch_template.tournamaths_lt.id
+    version = aws_launch_template.tournamaths_lt.latest_version # Specify this instead of "$Latest" so instance refresh triggered when launch template changes
   }
 
   # Just before an instance is terminated, give 300 seconds to perform any required actions,
@@ -314,8 +314,8 @@ resource "aws_route53_record" "tournamaths_a_record" {
   type    = "A"
 
   alias {
-    name                   = aws_lb.tourna_math_alb.dns_name
-    zone_id                = aws_lb.tourna_math_alb.zone_id
+    name                   = aws_lb.tournamaths_alb.dns_name
+    zone_id                = aws_lb.tournamaths_alb.zone_id
     evaluate_target_health = true
   }
 }
@@ -328,7 +328,7 @@ resource "aws_acm_certificate" "tournamaths_cert" {
 }
 
 resource "aws_lb_listener" "https_listener" {
-  load_balancer_arn = aws_lb.tourna_math_alb.arn
+  load_balancer_arn = aws_lb.tournamaths_alb.arn
   port              = 443
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
@@ -336,7 +336,7 @@ resource "aws_lb_listener" "https_listener" {
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.tourna_math_tg.arn
+    target_group_arn = aws_lb_target_group.tournamaths_tg.arn
   }
 
   # Want this listener created after SSL certificate validated, otherwise can get this sort of error:
@@ -397,20 +397,20 @@ resource "aws_acm_certificate_validation" "tournamaths_cert_validation" {
 
 ################ Database.
 # We're not doing backups here, deletion protection, SSL communication or encryption at rest, as just a pet project and keeping things simple.
-resource "aws_db_instance" "tourna_math_db" {
+resource "aws_db_instance" "tournamaths_db" {
   identifier                  = "tournamath-db"
   allocated_storage           = 20
   storage_type                = "gp2"
   engine                      = "postgres"
   engine_version              = "15.3"
   instance_class              = "db.t4g.micro"
-  db_name                     = "tourna_math_db"
+  db_name                     = "tournamaths_db"
   username                    = "admin_user" # Can't say "admin" here as that's reserved in Postgres.
   manage_master_user_password = true
   parameter_group_name        = "default.postgres15"
   skip_final_snapshot         = true
-  vpc_security_group_ids      = [aws_security_group.tourna_math_rds_sg.id]
-  db_subnet_group_name        = aws_db_subnet_group.tourna_math_private_db_subnet_group.name
+  vpc_security_group_ids      = [aws_security_group.tournamaths_rds_sg.id]
+  db_subnet_group_name        = aws_db_subnet_group.tournamaths_private_db_subnet_group.name
 
   apply_immediately = true # For convenience changes applied immediately, but should be careful
 }
