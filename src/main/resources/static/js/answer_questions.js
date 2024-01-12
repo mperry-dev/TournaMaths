fbf793084f63b77d4997a3e9a4b28af37b79b882
mperry-dev <mperry_48@outlook.com>
2024-01-12 19:17:38 +1000

    break up answer_questions.html into JS, CSS, HTML files

---
+++ src/main/resources/static/js/answer_questions.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    // Display the math problem
+    var mathProblem = 'c = \\pm\\sqrt{a^2 + b^2}';
+    $('#math-problem-box').html(katex.renderToString(mathProblem, {throwOnError: false}));
+
+    // Timer functionality
+    var timeLeft = 60;
+    var timerId = setInterval(countdown, 1000);
+
+    function countdown() {
+        if (timeLeft == 0) {
+            clearTimeout(timerId);
+            // Handle time-out scenario
+        } else {
+            $('#time-left').text(timeLeft);
+            timeLeft--;
+        }
+    }
+
+    // Handle answer submission
+    $('#submit-answer').click(function() {
+        var userAnswer = $('#answer').val();
+        // Add logic to check the answer and update the score
+    });
+});
