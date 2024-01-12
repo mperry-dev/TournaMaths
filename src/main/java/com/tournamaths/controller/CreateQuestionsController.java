948e6e9af957712f5df0e056656f829bc790e6c0
mperry-dev <mperry_48@outlook.com>
2024-01-13 00:30:55 +1000

    use Spring persistence context to manage session

--- src/main/java/com/tournamaths/controller/CreateQuestionsController.java
+++ src/main/java/com/tournamaths/controller/CreateQuestionsController.java
@@ -2,6 +2,7 @@ package com.tournamaths.controller;
 
 import com.tournamaths.entity.MathQuestion;
 
+import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Root;
@@ -9,9 +10,7 @@ import jakarta.persistence.criteria.Root;
 import java.util.List;
 
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
@@ -21,32 +20,28 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
-@Transactional // This means Spring handles committing/rolling back transaction automatically for public methods. Default propagation is REQUIRED
+@Transactional // This means Spring handles committing/rolling back transaction, and opening/closing session automatically for public methods. Default propagation is REQUIRED.
 public class CreateQuestionsController {
 
-    @Autowired
-    private SessionFactory sessionFactory;
+    @PersistenceContext
+    private Session session; // Use Spring's dependency injection to injection session object, and tied to current transaction context.
 
     @PostMapping("/create_questions")
     public String newQuestion(@RequestParam String identifier, @RequestParam String description, @RequestParam String equation, RedirectAttributes redirectAttributes) {
-        Session session = sessionFactory.openSession();
         MathQuestion newQuestion = new MathQuestion(identifier, description, equation);
         session.persist(newQuestion);
-        session.close();
         redirectAttributes.addFlashAttribute("message", "Question created successfully!");
         return "redirect:/create_questions";
     }
 
     @GetMapping("/create_questions")
     public String all(Model model) {
-        Session session = sessionFactory.openSession();
         CriteriaBuilder cb = session.getCriteriaBuilder();
         CriteriaQuery<MathQuestion> cq = cb.createQuery(MathQuestion.class);
         Root<MathQuestion> root = cq.from(MathQuestion.class);
         cq.select(root);
         Query<MathQuestion> query = session.createQuery(cq);
         List<MathQuestion> questions = query.getResultList();
-        session.close();
 
         model.addAttribute("questions", questions);
         return "create_questions";
