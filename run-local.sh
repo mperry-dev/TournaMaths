c94502d260c6395b30979d77c5a06f710e1036c0
mperry-dev <mperry_48@outlook.com>
2024-01-13 19:51:27 +1000

    tweaks

--- run-local.sh
+++ run-local.sh
@@ -4,7 +4,7 @@ mvn clean install
 docker-compose down
 
 # Build the Docker image. Need to rerun when rebuild java package since it grabs JAR.
-docker-compose --profile dev build -f docker/run/Dockerfile
+docker-compose --profile dev build tournamaths-app-run
 
 docker-compose --profile dev up -d db
 docker-compose --profile dev up tournamaths-app-run
