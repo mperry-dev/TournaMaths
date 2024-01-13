f2ad37357a215b2c1c7bf9d7df09ca2c73960c5c
mperry-dev <mperry_48@outlook.com>
2024-01-13 22:46:26 +1000

    undo autowiring CriteriaBuilder

--- src/main/java/com/tournamaths/config/MainConfig.java
+++ src/main/java/com/tournamaths/config/MainConfig.java
@@ -1,43 +1,10 @@
 package com.tournamaths.config;
 
-import org.hibernate.Session;
-import org.hibernate.query.criteria.HibernateCriteriaBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import jakarta.persistence.PersistenceContext;
 
 @Configuration
 @EnableTransactionManagement
 public class MainConfig {
-    /*
-     * Need to be careful not to use this outside of the transactional context,
-     * otherwise it can be stale.
-     */
 
-    // PersistenceContext used to inject Session/EntityManager
-    // Note Session subclasses EntityManager - a Hibernate Session is an EntityManager https://docs.jboss.org/hibernate/orm/6.4/javadocs/org/hibernate/Session.html
-    // If want thread-safety, need to use SessionFactory: https://www.baeldung.com/hibernate-entitymanager
-    // Useful info:
-    // https://github.com/hibernate/hibernate-orm/blob/main/hibernate-core/src/main/java/org/hibernate/Session.java
-    // https://github.com/hibernate/hibernate-orm/blob/main/hibernate-core/src/main/java/org/hibernate/SharedSessionContract.java
-    // https://github.com/hibernate/hibernate-orm/blob/main/hibernate-core/src/main/java/org/hibernate/internal/SessionImpl.java
-    // https://github.com/hibernate/hibernate-orm/blob/main/hibernate-core/src/main/java/org/hibernate/engine/spi/SessionImplementor.java
-    // https://github.com/hibernate/hibernate-orm/blob/dfa9cd5b2945f8384535853a54d836d121eb9067/hibernate-core/src/main/java/org/hibernate/engine/spi/SessionFactoryDelegatingImpl.java#L344
-    // https://github.com/hibernate/hibernate-orm/blob/dfa9cd5b2945f8384535853a54d836d121eb9067/hibernate-core/src/main/java/org/hibernate/query/criteria/spi/HibernateCriteriaBuilderDelegate.java#L88
-    @PersistenceContext
-    private Session session;
-
-    // https://www.baeldung.com/spring-bean
-    // In SessionFactoryImpl.java, the criteria builder is obtained from the query engine, which in turn creates it in QueryEngineImpl.java
-    // It looks like in , the session just talks to the sessionFactory to get the criteriaBuilder.
-    // Concretely, the CriteriaBuilder is of class SqmCriteriaNodeBuilder
-    // QueryEngineImpl (a query engine) -> creates SqmCriteriaNodeBuilder (a criteria builder)
-    // AbstractSharedSessionContract (a session) -> has a reference to a SessionFactoryImpl (a session factory)
-    // SessionFactoryImpl talks to QueryEngineImpl to get CriteriaBuilder in getCriteriaBuilder, after validating that not closed... so I'm not keen to skip that validation
-    @Bean
-    public HibernateCriteriaBuilder criteriaBuilder() {
-        return session.getCriteriaBuilder();
-    }
 }
--- src/main/java/com/tournamaths/controller/CreateQuestionsController.java
+++ src/main/java/com/tournamaths/controller/CreateQuestionsController.java
@@ -2,16 +2,15 @@ package com.tournamaths.controller;
 
 import com.tournamaths.entity.MathQuestion;
 
+import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Root;
 
 import java.util.List;
 
-import org.hibernate.Session;
-import org.hibernate.query.Query;
-import org.hibernate.query.criteria.HibernateCriteriaBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
@@ -23,17 +22,18 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 /**
  * Transactional means Spring handles committing/rolling back transaction, and opening/closing session automatically for public methods.
  * https://stackoverflow.com/questions/24710620/how-does-transactional-influence-current-session-in-hibernate
- * Default propagation is REQUIRED - if a transactional method calls another transactional method, the existing transaction continues.
- * It's important to this propagation approach since doing dependency injection of Session and CriteriaBuilder - want to avoid them being used in the wrong transaction.
+ * Default propagation = REQUIRED ; if a transactional method calls another transactional method, the existing transaction continues.
+ * It's important to use this propagation approach since doing dependency injection of Session - want to avoid them being used in the wrong transaction.
  * Rolling back by default is only for Runtime exceptions (not checked exceptions).
- * NOTE because using Transactional, have container-managed transactions, so only need to rollback after unchecked exceptions https://docs.jboss.org/hibernate/orm/6.4/javadocs/org/hibernate/Session.html
+ * Because using Transactional, have container-managed transactions, so only need to rollback after unchecked exceptions:
+ * https://docs.jboss.org/hibernate/orm/6.4/javadocs/org/hibernate/Session.html
  */
 @Controller
 @Transactional
 public class CreateQuestionsController {
 
-    // Use Spring's dependency injection to injection session object, and tied to current transaction context.
-    // Much nicer than having to Autowire and use SessionFactory!
+    // Use Spring's dependency injection to inject entityManager object, and tied to current transaction context.
+    // Much nicer than having to Autowire and use EntityManagerFactory!
     // Need to be careful not to use this outside of the transactional context,
     // otherwise it can be stale.
     // Useful docs:
@@ -42,31 +42,27 @@ public class CreateQuestionsController {
     // https://stackoverflow.com/questions/31335211/autowired-vs-persistencecontext-for-entitymanager-bean
     // https://stackoverflow.com/questions/5640778/hibernate-sessionfactory-vs-jpa-entitymanagerfactory
     @PersistenceContext
-    private Session session;
-
-    // Need to be careful not to use this outside of the transactional context,
-    // otherwise it can be stale.
-    // Code shows how CriteriaBuilder reaches through SessionFactory when creating queries:
-    // final SqmStatement<T> statement = sessionFactory.get().getQueryEngine().getHqlTranslator().translate( hql, resultClass );
-    // https://github.com/hibernate/hibernate-orm/blob/dfa9cd5b2945f8384535853a54d836d121eb9067/hibernate-core/src/main/java/org/hibernate/query/sqm/internal/SqmCriteriaNodeBuilder.java#L320
-    // SessionFactoryImpl.java -> queryEngine = QueryEngineImpl.from( this, bootMetamodel );
-    @Autowired
-    private HibernateCriteriaBuilder cb;
+    private EntityManager entityManager;
 
     @PostMapping("/create_questions")
     public String newQuestion(@RequestParam String identifier, @RequestParam String description, @RequestParam String equation, RedirectAttributes redirectAttributes) {
         MathQuestion newQuestion = new MathQuestion(identifier, description, equation);
-        session.persist(newQuestion);
+        entityManager.persist(newQuestion);
         redirectAttributes.addFlashAttribute("message", "Question created successfully!");
         return "redirect:/create_questions";
     }
 
     @GetMapping("/create_questions")
     public String all(Model model) {
+        // Even though this is boilerplate, it is best to not autowire in since Hibernate does some validation when running getCriteriaBuilder - see:
+        // https://github.com/hibernate/hibernate-orm/blob/main/hibernate-core/src/main/java/org/hibernate/internal/SessionFactoryImpl.java#L811
+        // This also ensures that the criteria builder is part of the current transaction context and persistence context.
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+
         CriteriaQuery<MathQuestion> cq = cb.createQuery(MathQuestion.class);
         Root<MathQuestion> root = cq.from(MathQuestion.class);
         cq.select(root);
-        Query<MathQuestion> query = session.createQuery(cq);
+        TypedQuery<MathQuestion> query = entityManager.createQuery(cq);
         List<MathQuestion> questions = query.getResultList();
 
         model.addAttribute("questions", questions);
