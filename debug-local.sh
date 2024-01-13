d9561f4e7035342e0248d0d4068239e4adbf9eea
mperry-dev <mperry_48@outlook.com>
2024-01-13 20:08:47 +1000

    tweak local build

--- debug-local.sh
+++ debug-local.sh
@@ -1,6 +1,9 @@
 # Generate application JAR file.
 mvn clean install
 
+# Move JAR into build context
+cp target/tournamaths-1.0.jar docker/debug/
+
 docker-compose down
 
 # Build the Docker image. Need to rerun when rebuild java package since it grabs JAR.
