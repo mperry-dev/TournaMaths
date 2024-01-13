fc29369849dfd6e2c14a08185b3aee7998c89d53
mperry-dev <mperry_48@outlook.com>
2024-01-13 18:44:37 +1000

    copy run-local.sh to debug-local.sh

---
+++ debug-local.sh
@@ -0,0 +1,10 @@
+# Generate application JAR file.
+mvn clean install
+
+docker-compose down
+
+# Build the Docker image. Need to rerun when rebuild java package since it grabs JAR.
+docker-compose --profile dev build
+
+docker-compose --profile dev up -d db
+docker-compose --profile dev up tournamaths-app
