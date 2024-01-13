483e3adaecd9a44e783eeaa3c0cc7062b2fc04a9
mperry-dev <mperry_48@outlook.com>
2024-01-13 16:16:32 +1000

    add more links

--- src/main/java/com/tournamaths/config/MainConfig.java
+++ src/main/java/com/tournamaths/config/MainConfig.java
@@ -19,9 +19,17 @@ public class MainConfig {
     // PersistenceContext used to inject Session/EntityManager
     // Note Session subclasses EntityManager - a Hibernate Session is an EntityManager https://docs.jboss.org/hibernate/orm/6.4/javadocs/org/hibernate/Session.html
     // If want thread-safety, need to use SessionFactory: https://www.baeldung.com/hibernate-entitymanager
+    // Useful info:
+    // https://github.com/hibernate/hibernate-orm/blob/main/hibernate-core/src/main/java/org/hibernate/Session.java
+    // https://github.com/hibernate/hibernate-orm/blob/main/hibernate-core/src/main/java/org/hibernate/SharedSessionContract.java
+    // https://github.com/hibernate/hibernate-orm/blob/main/hibernate-core/src/main/java/org/hibernate/internal/SessionImpl.java
+    // https://github.com/hibernate/hibernate-orm/blob/main/hibernate-core/src/main/java/org/hibernate/engine/spi/SessionImplementor.java
+    // https://github.com/hibernate/hibernate-orm/blob/dfa9cd5b2945f8384535853a54d836d121eb9067/hibernate-core/src/main/java/org/hibernate/engine/spi/SessionFactoryDelegatingImpl.java#L344
+    // https://github.com/hibernate/hibernate-orm/blob/dfa9cd5b2945f8384535853a54d836d121eb9067/hibernate-core/src/main/java/org/hibernate/query/criteria/spi/HibernateCriteriaBuilderDelegate.java#L88
     @PersistenceContext
     private Session session;
 
+    // https://www.baeldung.com/spring-bean
     @Bean
     public HibernateCriteriaBuilder criteriaBuilder() {
         return session.getCriteriaBuilder();
