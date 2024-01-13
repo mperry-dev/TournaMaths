ab53fdf341c7f4468e872cb7d863c3bd4438b8ec
mperry-dev <mperry_48@outlook.com>
2024-01-13 12:59:52 +1000

    delete redundant imports

--- src/main/java/com/tournamaths/config/MainConfig.java
+++ src/main/java/com/tournamaths/config/MainConfig.java
@@ -3,11 +3,8 @@ package com.tournamaths.config;
 import org.hibernate.Session;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.criteria.CriteriaBuilder;
 
