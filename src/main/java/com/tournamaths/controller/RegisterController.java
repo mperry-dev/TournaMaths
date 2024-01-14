05deff53a2123d5d0cf3bb8746edb52bb2550597
mperry-dev <mperry_48@outlook.com>
2024-01-14 23:38:21 +1000

    add registration controller

---
+++ src/main/java/com/tournamaths/controller/RegisterController.java
@@ -0,0 +1,27 @@
+package com.tournamaths.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.tournamaths.entity.User;
+import com.tournamaths.repository.UserRepository;
+
+@Controller
+public class RegisterController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+        return ResponseEntity.ok("User registered successfully");
+    }
+}
