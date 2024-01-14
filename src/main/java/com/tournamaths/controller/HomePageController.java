30354147fd1f0ca02025968f7e0772cb963b2696
mperry-dev <86701525+mperry-dev@users.noreply.github.com>
2024-01-14 16:41:04 +1000

    Create sign up page (viewable only) (#17)

---
+++ src/main/java/com/tournamaths/controller/HomePageController.java
@@ -0,0 +1,12 @@
+package com.tournamaths.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomePageController {
+    @GetMapping("/")
+    public String home() {
+        return "sign_up";
+    }
+}
