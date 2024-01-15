8e29df246d18865764405adea1a935b2e76a1059
mperry-dev <mperry_48@outlook.com>
2024-01-15 23:12:42 +1000

    redirect to /create_questions page upon signup

--- src/main/java/com/tournamaths/controller/RegisterController.java
+++ src/main/java/com/tournamaths/controller/RegisterController.java
@@ -1,19 +1,21 @@
 package com.tournamaths.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 import com.tournamaths.entity.AppUser;
 import com.tournamaths.repository.AppUserRepository;
 
+import jakarta.servlet.http.HttpSession;
 import jakarta.transaction.Transactional;
 
 @RestController
@@ -29,7 +31,7 @@ public class RegisterController {
     private AuthenticationManager authenticationManager;
 
     @PostMapping("/register")
-    public ResponseEntity<?> registerUser(@ModelAttribute AppUser user) {
+    public RedirectView registerUser(@ModelAttribute AppUser user, HttpSession session) {
         String password = user.getPassword();
 
         // Add user to database
@@ -43,6 +45,10 @@ public class RegisterController {
         // Set the authentication in the security context, including granted authorities and other details
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
-        return ResponseEntity.ok("User registered and logged in successfully");
+        // Set the authentication in the user's session
+        session.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY, SecurityContextHolder.getContext());
+
+        // Redirect to /create_questions page (RestController-specific way)
+        return new RedirectView("/create_questions");
     }
 }
