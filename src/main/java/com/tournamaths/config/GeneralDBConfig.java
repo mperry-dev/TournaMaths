4b482abaec8e10c80ec9a7d99a0324fdbc95b017
mperry-dev <mperry_48@outlook.com>
2024-01-20 17:02:49 +1000

    MainConfig -> GeneralDBConfig

--- src/main/java/com/tournamaths/config/GeneralDBConfig.java
+++ src/main/java/com/tournamaths/config/GeneralDBConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-@EnableTransactionManagement
-public class MainConfig {
+@EnableTransactionManagement // Enable Spring's annotation-driven transaction management capability, across all environments
+public class GeneralDBConfig {
 
 }
