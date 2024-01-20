886ea4bf6bad1a5db89fa34ff2cf929a3844c611
mperry-dev <mperry_48@outlook.com>
2024-01-20 22:47:58 +1000

    fix parameter group

--- infrastructure/cache.tf
+++ infrastructure/cache.tf
@@ -5,8 +5,8 @@ resource "aws_elasticache_cluster" "tournamaths_elasticache_redis" {
   engine               = "redis"
   node_type            = "cache.t4g.micro"
   num_cache_nodes      = 1 # Keeping low so cheap
-  parameter_group_name = "default.redis7.0"
-  engine_version       = "7.0"
+  parameter_group_name = "default.redis7" # Cluster mode is disabled https://docs.aws.amazon.com/AmazonElastiCache/latest/red-ug/ParameterGroups.Redis.html#ParameterGroups.Redis.7
+  engine_version       = "7.1"
   port                 = 6379
   subnet_group_name    = aws_elasticache_subnet_group.tournamaths_private_elasticache_subnet_group.name
   security_group_ids   = [aws_security_group.tournamaths_elasticache_sg.id]
