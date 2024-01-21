9ff3fb4b17bd52f742b0fa48fdc95f46202ca16a
mperry-dev <mperry_48@outlook.com>
2024-01-21 21:48:20 +1000

    add es6 plugin to eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
     // Your ESLint configuration here
-    extends: 'eslint:recommended',
+    extends: ['eslint:recommended', 'plugin:es6/recommended'],
     rules: {
       // Your ESLint rules here
     },
